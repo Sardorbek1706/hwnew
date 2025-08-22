c20dc056a19ab0687f28ca5a7af6a018f8637b4e
Sardorbek1706 <sardorbektursunboyev06@gmail.com>
2025-08-22 21:28:02 +0500

    Delete 6.js

--- 6.js
+++
@@ -1,7 +0,0 @@
-function doubleChar(str) {
-  return str.split('').map(char => char.repeat(2)).join('');
-}
-
-console.log(doubleChar("String")); 
-console.log(doubleChar("Hello World!")); 
-console.log(doubleChar("1234!_ ")); 
