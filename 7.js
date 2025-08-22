6deed62684e520d204c048a43112a2502f658991
Sardorbek1706 <sardorbektursunboyev06@gmail.com>
2025-08-22 21:28:15 +0500

    Delete 7.js

--- 7.js
+++
@@ -1,7 +0,0 @@
-function checkTitle(str) {
-  return str.split(' ').every(word => word[0] === word[0].toUpperCase());
-}
-
-console.log(checkTitle("A Mind Boggling Achievement")); 
-console.log(checkTitle("A Simple Java Script Program!")); 
-console.log(checkTitle("Water is transparent"));
